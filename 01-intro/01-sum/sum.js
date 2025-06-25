797e1c3e84bf699217c7289464bd6e94ce85aaff
Anna Vostrykova <it4anna@gmail.com>
2025-06-25 16:03:04 -0400

    Lesson-01: Home Work

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,3 +1,5 @@
 export default function sum(a, b) {
-  /* ваш код */
+    const checkNumber = (arg) => typeof arg === 'number'
+    if(!checkNumber(a) || !checkNumber(b)) throw new TypeError()
+    return a+b
 }
