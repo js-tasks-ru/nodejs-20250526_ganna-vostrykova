8fa68899303ac80bef2ef8882474486c81ac5abd
Hanna Vostrykova <it4anna@gmail.com>
2025-07-08 18:47:52 -0400

    Home work 01: Tasks with validation

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -1,17 +1,36 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { Task } from "./task.model";
+import { Injectable, NotFoundException } from "@nestjs/common"
+import { v6 as uuidv6 } from 'uuid'
+import {Task} from "./task.model"
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
+  private tasks: Task[] = []
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string): Task|undefined {
+    const task = this.tasks.find(u => u.id === id)
+    if (!task) throw new NotFoundException(`task with id ${id} is not found`)
+    return task
+  }
 
-  createTask(task: Task): Task {}
+  createTask(data: Task): Task {
+    const task = {...data, id: uuidv6()}
+    this.tasks.push(task)
+    return task
+  }
 
-  updateTask(id: string, update: Task): Task {}
+  updateTask(id: string, update: Task): Task|undefined {
+    const task = this.getTaskById(id)
+    if(task) Object.assign(task, update)// Apply updates
+    return task
+  }
 
-  deleteTask(id: string): Task {}
+  deleteTask(id: string): Task {
+    const task = this.getTaskById(id)
+    if(task) this.tasks = this.tasks.filter(task => task.id !== id)
+    return task
+  }
 }
